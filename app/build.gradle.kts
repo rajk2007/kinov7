bc69f5233348d053a51cb0edc829e2c210f1c80e
rajk2007 <rajk2007@users.noreply.github.com>
2026-06-21 06:11:17 +0000

    feat: initial kino branding and amoled theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -101,7 +101,7 @@ android {
     compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "com.lagradost.cloudstream3"
+        applicationId = "com.rajk2007.kino"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = libs.versions.versionCode.get().toInt()
